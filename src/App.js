e99fd33fb043a5fafbd5e3f25257372cbcbe8bf0
ifaqer <super.mr.remix@gmail.com>
2023-12-16 01:14:09 +0300

    my code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './index.scss';
+import React from 'react'
 
 const questions = [
   {
@@ -22,37 +23,47 @@ const questions = [
   },
 ];
 
-function Result() {
+function Result({result, setResult, setStep, setFinal}) {
   return (
     <div className="result">
       <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-      <h2>Вы отгадали 3 ответа из 10</h2>
-      <button>Попробовать снова</button>
+      <h2>{`Вы отгадали ${result} ответа из 3`}</h2>
+      <button onClick={()=>{
+        setStep(0)
+        setResult(0)
+        setFinal(false)
+      }}>Попробовать снова</button>
     </div>
   );
 }
 
-function Game() {
+function Game({step, setStep, question, result, setResult, setFinal}) {
   return (
     <>
       <div className="progress">
-        <div style={{ width: '50%' }} className="progress__inner"></div>
+        <div style={{width: `${(step/3*100)}%`}} className="progress__inner"></div>
       </div>
-      <h1>Что такое useState?</h1>
+      <h1>{question.title}</h1>
       <ul>
-        <li>Это функция для хранения данных компонента</li>
-        <li>Это глобальный стейт</li>
-        <li>Это когда на ты никому не нужен</li>
+        {question.variants.map((key, index)=><li onClick={()=>{
+          if (index == question.correct){setResult(result + 1)}
+          setStep(step + 1)
+          if (step == 2){setFinal(true)}
+          }}>{key}</li>)}
       </ul>
     </>
   );
 }
 
 function App() {
+  const [step, setStep] = React.useState(0)
+  const [result, setResult] = React.useState(0)
+  const [final, setFinal] = React.useState(false)
+  const question = questions[step]
   return (
     <div className="App">
-      <Game />
-      {/* <Result /> */}
+      {!final && <Game step={step} setStep={setStep} question={question} result={result} setFinal={setFinal} setResult={setResult}/>}
+      {final && <Result result={result} setResult={setResult} setStep={setStep} setFinal={setFinal}/>}
     </div>
   );
 }
