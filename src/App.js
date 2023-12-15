b0ff0c7fdf1838d776328cbfe878310e0626fb94
ifaqer <super.mr.remix@gmail.com>
2023-12-15 20:13:56 +0300

    add children content

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './index.scss';
 
-function Modal(props){
+function Modal({open, setOpen, children}){
   return(
-    <div className={`overlay animated ${props.open ? "show" : ""}`}>
+    <div className={`overlay animated ${open ? "show" : ""}`}>
         <div className="modal">
-          <svg onClick={() => {props.setOpen(false)}} height="200" viewBox="0 0 200 200" width="200">
-            <title />
+          <svg onClick={() => {setOpen(false)}} height="200" viewBox="0 0 200 200" width="200">
             <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
           </svg>
+          {children}
           <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+          
         </div>
       </div>
   )
@@ -20,7 +21,9 @@ function App() {
   return (
     <div className="App">
       <button className="open-modal-btn" onClick={() => {setOpen(true)}}>✨ Открыть окно</button>
-      <Modal open={open} setOpen={setOpen}/>
+      <Modal open={open} setOpen={setOpen}>
+        <p>Динамический контент, правда?</p>
+      </Modal>
     </div>
   );
 }
