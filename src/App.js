e75dcd620a0af63e66b2c9c3536477a47e91f925
ifaqer <super.mr.remix@gmail.com>
2023-12-15 19:54:47 +0300

    asd

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './index.scss';
+import React from 'react'
 
 function App() {
+  const [number, setNumber] = React.useState(0)
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{number}</h1>
+        <button className="minus" onClick={() => {setNumber(number+1)}}>- Минус</button>
+        <button className="plus" onClick={() => {setNumber(number-1)}}>Плюс +</button>
       </div>
     </div>
   );
